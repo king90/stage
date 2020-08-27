f7cc179f6b80ae0da7f96579fa6c5c92ccf11bd2
nnzhu <king@qq.com>
2020-08-27 23:04:55 +0800

    feat: demo

---
+++ rollup-stage/meta.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "prompts": {
+    	"name": {
+        	"type": "string",
+        	"required": true,
+	       "message" : "Project name"
+	    },
+	    "test": {
+        	"type": "confirm",
+	       "message" : "Unit test?"
+	    },
+	    "version": {
+	       "type": "input",
+	       "message": "project's version",
+	       "default": "1.0.0"
+	    }
+    }
+}
